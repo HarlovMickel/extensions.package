4e280b22f81d5e882235dbf9249ade899da4654d
Mickel <harlovm@gmail.com>
2021-11-09 17:37:02 +0700

    Правки по модификаторам доступа

--- Sources/UIKitExtension/UIStoryboard+Extension.swift
+++ Sources/UIKitExtension/UIStoryboard+Extension.swift
@@ -10,12 +10,12 @@ import UIKit
 // MARK: UIStoryboard (Factory)
 public extension UIStoryboard {
     
-    public static func initialViewController(with storyboardName: String, bundle: Bundle? = nil) -> UIViewController? {
+    static func initialViewController(with storyboardName: String, bundle: Bundle? = nil) -> UIViewController? {
         let storyboard = UIStoryboard(name: storyboardName, bundle: bundle)
         return storyboard.instantiateInitialViewController()
     }
     
-    public static func controller(with storyboardName: String, identifier: String, bundle: Bundle? = nil) -> UIViewController? {
+    static func controller(with storyboardName: String, identifier: String, bundle: Bundle? = nil) -> UIViewController? {
         let storyboard = UIStoryboard(name: storyboardName, bundle: bundle)
         return storyboard.instantiateViewController(withIdentifier: identifier)
     }
